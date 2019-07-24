8def8c754cfb54f350696a7bc0882a596111a624
ChandanaRD <chanrd1996@gmail.com>
2019-07-24 15:31:30 +0530

    input for item field added no item page added edit, save, delete add completed

--- shopping_expense_manager_UI/src/components/ItemBox.js
+++ shopping_expense_manager_UI/src/components/ItemBox.js
@@ -6,39 +6,34 @@ class ItemBox extends React.Component{
 
     constructor(props){
         super(props);
-        this.state={};
+        this.state={
+            'itemTitle':this.props.content.title
+        };
     }
 
-    // editItem = ()=>{
-    //     console.log(this.props.content);
-    // }
-
-    // itemDone = () =>{
-    //     console.log('item done.');
-    // }
-
-    // deleteItem = () =>{
-    //     console.log('deleting...');
-    // }
+    componentWillReceiveProps(nextProps) {
+        // You don't have to do this check first, but it can help prevent an unneeded render
+        if (nextProps.content.title !== this.state.itemTitle) {
+          this.setState({ itemTitle: nextProps.content.title });
+        }
+      }
 
     render(){
         let editItem = this.props.onEdit;
-        let itemDone = this.props.onDone;
+        let itemDoneToggle = this.props.onDoneToggle;
         let deleteItem = this.props.onDelete;
-        return (<div className='ItemBox'>
-            <div className='ui fluid card '>
-                <div className="content"><div className='header align'> 
-                    <div className={this.props.done?'strikeThrough':''}>
-                        {this.props.content}
+        // let newItemTitle =null;
+        return (<div className='ItemBox ui fluid card '>
+                <div className="content ui form">
+                    <div className='field align'>
+                        <input id={this.props.content.id} className={this.props.content.done?'strikeThrough item-content':'item-content'} type="text" placeholder="Click on edit icon to add description" disabled={this.props.content.disabled} value = {this.state.itemTitle} onChange={(e)=>{this.setState({'itemTitle':e.target.value})}}/>
+                        <div className='ui buttons display-inline'>
+                            <button className="ui button primary basic" onClick={()=>{editItem(this.props.content.id, this.state.itemTitle)}}><i className={this.props.content.disabled?'icon pencil alternate':'icon save'}></i></button>
+                            <button className="ui button primary basic" onClick={()=>{itemDoneToggle(this.props.content.id)}}><i className={this.props.content.done?'icon undo':'icon check'}></i></button>
+                            <button className="ui button primary basic" onClick={()=>{deleteItem(this.props.content.id)}}><i className="icon x icon"></i></button>
+                        </div>
                     </div>
-                    <div className='ui buttons'>
-                        <button className="ui button primary basic" onClick={()=>{editItem(this.props.content)}}><i className="icon pencil alternate"></i></button>
-                        <button className="ui button primary basic" onClick={()=>{itemDone(this.props.content)}}><i className="icon check"></i></button>
-                        <button className="ui button primary basic" onClick={()=>{deleteItem(this.props.content)}}><i className="icon x icon"></i></button>
-                    </div>
-                </div> 
             </div>
-        </div>
     </div>)
     }
 }
