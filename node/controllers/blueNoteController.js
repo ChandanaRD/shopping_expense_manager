d47b66faa5f46c3fe095281615083f960ae58b74
ChandanaRD <chanrd1996@gmail.com>
2019-07-28 15:10:34 +0530

    apis complete

--- node/controllers/blueNoteController.js
+++ node/controllers/blueNoteController.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 var note = require('../models/blueNote.model.js');
 var router = Express.Router({mergeParams:true});
 
+mongoose.set('useFindAndModify', false);
+
 router.get('/', (req, res)=>{
     res.send("Hey! welcome!")
 });
@@ -19,7 +21,10 @@ router.get('/getAll',async (req, res)=>{
 });
 
 router.post('/addOne',async (req,res)=>{
+    //format => inside req.body should contain : {"id":"n1","title":"item1","done":false,"disabled":true}
+    console.log("addone");
     try{
+        console.log(req.body);
         var newNote= new note(req.body);
         var result = newNote.save(function (err) {
             if (err) return handleError(err);
@@ -34,14 +39,17 @@ router.post('/addOne',async (req,res)=>{
 });
 
 router.post('/addMultiple',async (req,res)=>{
+    //format => inside req.body should contain : [{"id":"n1","title":"item1","done":false,"disabled":true}]
     try{
         var newNote= new note(req.body);
-        var result = newNote.collection.insertMany(req.body,function (err) {
-            if (err) return console.log(err);
+        var result = await newNote.collection.insertMany(req.body,function (err) {
+            if (err) {
+                console.log("could not insert document");
+                res.send(err);
+            }
         });
-        var list = await note.find().exec();
-        res.send(list);
-        console.log(req.body.length+" documents inserted!");
+        res.send('inserted');
+        console.log('inserted');
     }catch(err){
         res.status(500).send(err);
         console.log('could not create');
@@ -49,20 +57,27 @@ router.post('/addMultiple',async (req,res)=>{
 });
 
 router.put('/editNote:id', async(req, res)=>{
+    //reqURL : http://localhost:3001/allNotes/editNoten2
     try{
-        var editNote = await note.find({'id':req.params.id}).exec();
-        console.log(req.params.id)
-        editNote.set(req.body);
-        var result = await note.save();
+        note.findOneAndUpdate({id:req.params.id},{$set: req.body},function(err,doc){
+            if (err) {
+                console.log("update document error");
+            } else {
+                console.log("update document success");
+                res.send('the following document: '+JSON.stringify(doc)+' \n is updated to: '+JSON.stringify(req.body));
+            }
+        })
         result = await note.find().exec();
-        console.log(result);
-        res.send(result);
+        // console.log(result);
+        // res.send(result);
     }catch(err){
         res.status(500).send(err);
+        console.log(err);
     }
 })
 
 router.delete("/deleteNote:id", async (req, res)=>{
+    //reqURL : http://localhost:3001/allNotes/deleteNoten2
     try{
         var result = await note.deleteOne({id:req.params.id}).exec();
         res.send(result);
