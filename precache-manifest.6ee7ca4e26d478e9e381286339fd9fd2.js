6015e18c0269d04851ba4bd9180f5b9e90efd833
ChandanaRD <chanrd1996@gmail.com>
2019-07-29 14:50:54 +0530

    Updates

---
+++ precache-manifest.6ee7ca4e26d478e9e381286339fd9fd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1561ffad0de3487dfd0905c99600750a",
+    "url": "/shopping_expense_manager/index.html"
+  },
+  {
+    "revision": "2205f83e6439ed69d612",
+    "url": "/shopping_expense_manager/static/css/main.fcc823cd.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/shopping_expense_manager/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "2205f83e6439ed69d612",
+    "url": "/shopping_expense_manager/static/js/main.9d7f22a0.chunk.js"
+  },
+  {
+    "revision": "33d4d58a63d6d6910356",
+    "url": "/shopping_expense_manager/static/js/runtime~main.4c035ae3.js"
+  }
+]);
