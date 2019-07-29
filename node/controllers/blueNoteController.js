d6e7458945fe634d63423e8e9461b95aa2b26fbe
ChandanaRD <chanrd1996@gmail.com>
2019-07-29 14:30:33 +0530

    database linked

--- node/controllers/blueNoteController.js
+++ node/controllers/blueNoteController.js
@@ -26,22 +26,17 @@ router.post('/addNote',async (req,res)=>{
     //format => inside req.body should contain : [{"id":"n1","title":"item1","done":false,"disabled":true}]
     // can add one or more notes together
     try{
-        var i=0;
-        var newNote= new note(req.body);
-        var error = null;
-        var result = await newNote.collection.insertMany(req.body,function (err, result) {
+        await note.collection.insertMany(req.body, async function (err, result) {
             if (err) {
                 console.log(err);
-                error=err;
-                console.log(err);
-                res.status(210).send(err);
+                res.statusMessage="1 or more docs have duplicate IDs";
+                res.status(211).send(err);
             } else if(result){
-                console.log('no. of documents inserted: ' +req.body.length);
-                res.status(200).send(result);
-                console.log(result);
+                var list = await note.find().exec();
+                console.log(list);
+                res.status(200).send(list);
             }
         });
-        // console.log('inserted');
     }catch(err){
         res.status(500).send(err);
         console.log('could not create');
@@ -53,18 +48,29 @@ router.put('/editNote:id', async(req, res)=>{
     // req.body: {"id":"idVal"} or {"reqVield":"requiredVal"} one or more or all fields
     var filter = {id:req.params.id};
     var update = req.body;
-    var error = null;
-    var docToBeUpdated=null;
     try{
-        await note.findOneAndUpdate(filter,update,function(err,result){
-            if (err) {
-                console.log("update document error:\n"+err);
-                res.status(205).send(err);
-            } else if(result) {
-                console.log('the following document: '+JSON.stringify(result)+' \n is updated to: '+JSON.stringify(req.body))
-            res.status(200).send(result);
+        note.find(filter, async function (err,resNote){
+            if(err){
+                console.log("err\n"+err);
+                res.statusMessage="Unkonwn";
+                res.status(212).send(err);
+            }else if (resNote.length!=0){
+                await note.update(filter,update, async function(err,result){
+                    if (err) {
+                        console.log("update document error:\n"+err);
+                        res.status(205).send(err);
+                    } else if(result){
+                        console.log(result);
+                        var list = await note.find().exec();
+                        console.log(list);
+                        res.status(200).send(list);
+                    }
+                })
+            }else{
+                res.statusMessage = "1 or more id(s) not found";
+                res.status(210).end();
             }
-        })
+        });     
     }catch(err){
         res.status(500).send(err);
         console.log(err);
@@ -75,11 +81,18 @@ router.delete("/deleteNote", async (req, res)=>{
     //reqURL : http://localhost:3001/allNotes/deleteNote
     // req.body : ["n1","n2","n3"] or ["n1"]
     console.log(req.body);
-    var filter={'id':{$in:req.body}}
+    var filter={'id':{$in:req.body.ids}}
     try{
         var result = await note.deleteMany(filter).exec();
-        console.log(result);
-        res.status(200).send(result);
+        if(result.ok == 1 && result.n == 0){
+            console.log(result);
+            res.statusMessage = "1 or more id(s) not found";
+            res.status(210).end();
+        }else if(result.ok == 1  && result.n > 0){
+            var list = await note.find().exec();
+            console.log(list);
+            res.status(200).send(list);
+        }
     }catch(error){
         console.log(error);
         res.status(500).send(error);
--- shopping_expense_manager_UI/src/components/ShoppingList.js
+++ shopping_expense_manager_UI/src/components/ShoppingList.js
@@ -8,92 +8,215 @@ class ShoppingList extends React.Component {
         console.log('constructer called')
         super(props);
         this.state = {
-            'shoppingItemList': []
+            'shoppingItemList': [],
+            'isError': false,
+            'isLoading': false,
         };
+        this.header = new Headers({
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        });
     }
 
-    generateItemId = () =>{
+    generateItemId = () => {
         var newID = 0;
-        if(this.state.shoppingItemList.length>0){
-            this.state.shoppingItemList.forEach(({id})=>{
-                if(id==="it"+newID && newID<=this.state.shoppingItemList.length){
+        if (this.state.shoppingItemList.length > 0) {
+            this.state.shoppingItemList.forEach(({ id }) => {
+                if (id === "it" + newID && newID <= this.state.shoppingItemList.length) {
                     newID++;
-                }else{
-                    return "it"+newID;
+                } else {
+                    return "it" + newID;
                 }
             })
-        }else{
+        } else {
             return 'it0';
         }
     }
 
-    addItemToList = () =>{
+    addItemToList = () => {
         console.log('inside addItemList');
-        var  newShoppingItemList = this.state.shoppingItemList;
-        var id = 'it'+this.state.shoppingItemList.length;
-        newShoppingItemList.push({'id':id,'title':'', 'done':false, 'disabled':true});
-        this.setState({'shoppingItemList':newShoppingItemList});
+        var id = 'it' + this.state.shoppingItemList.length;
+        var addNote = new Request('http://localhost:3001/allNotes/addNote', {
+            method: 'post',
+            body: JSON.stringify([{ 'id': id, 'title': '', 'done': false, 'disabled': true }]),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
+
+        });
+        this.setState({ isLoading: true });
+        fetch(addNote)
+            .then(response => {
+                console.log(response)
+                response.json().then(data => {
+                    if (data)
+                        this.setState({ shoppingItemList: data, isLoading: false })
+                    else
+                        this.setState({ isLoading: false, isError: true })
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ isLoading: false, isError: true })
+            })
     }
 
-    editItem = (itemid, newval)=>{
+    editItem = (itemid, newval) => {
         console.log('inside editItem');
-        var itemIndex = this.state.shoppingItemList.findIndex(({id})=>{
+        var itemIndex = this.state.shoppingItemList.findIndex(({ id }) => {
             return id === itemid;
         });
         var newShoppingItemList = this.state.shoppingItemList.slice();
         if(!newShoppingItemList[itemIndex].disabled){
-            newShoppingItemList[itemIndex].title = newval?newval:this.state.shoppingItemList[itemIndex].title;
-            newShoppingItemList[itemIndex].disabled =true;
-        }else{
+            this.setState({ isLoading: true });
+            var editNote = new Request('http://localhost:3001/allNotes/editNote' + itemid, {
+                method: 'put',
+                body: JSON.stringify({ title: newval, disabled: true }),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                }
+
+            });
+            fetch(editNote)
+                .then(response => {
+                    console.log(response)
+                    response.json().then(data => {
+                        if (data)
+                            this.setState({ shoppingItemList: data, isLoading: false })
+                        else
+                            this.setState({ isLoading: false, isError: true })
+                    })
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.setState({ isLoading: false, isError: true })
+                })
+        } else {
             newShoppingItemList[itemIndex].disabled = false;
         }
-        this.setState({'shoppingItemList':newShoppingItemList})
+        this.setState({ 'shoppingItemList': newShoppingItemList })
     }
 
-    onDoneToggle = (itemid) =>{
+    onDoneToggle = (itemid) => {
         console.log('inside done');
-        var itemIndex = this.state.shoppingItemList.findIndex(({id})=>{
+        var itemIndex = this.state.shoppingItemList.findIndex(({ id }) => {
             return id === itemid;
         });
-        var newShoppingItemList = this.state.shoppingItemList.slice();
-        newShoppingItemList[itemIndex].done = newShoppingItemList[itemIndex].done?false:true;
-        newShoppingItemList[itemIndex].disabled = true; 
-        this.setState({'shoppingItemList':newShoppingItemList})
+        var noteDone = new Request('http://localhost:3001/allNotes/editNote' + itemid, {
+                method: 'put',
+                body: JSON.stringify({ disabled: true, done: !this.state.shoppingItemList[itemIndex].done }),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                }
+
+            });
+            fetch(noteDone)
+                .then(response => {
+                    console.log(response)
+                    response.json().then(data => {
+                        console.log('data');
+                        console.log(data);
+                        if (data)
+                            this.setState({ shoppingItemList: data, isLoading: false })
+                        else
+                            this.setState({ isLoading: false, isError: true })
+                    })
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.setState({ isLoading: false, isError: true })
+                })
+        // var newShoppingItemList = this.state.shoppingItemList.slice();
+        // newShoppingItemList[itemIndex].done = newShoppingItemList[itemIndex].done ? false : true;
+        // newShoppingItemList[itemIndex].disabled = true;
+        // this.setState({ 'shoppingItemList': newShoppingItemList })
     }
 
-    deleteItem = (itemid) =>{
+    deleteItem = (itemid) => {
         console.log('delete item');
-        var newShoppingItemList = this.state.shoppingItemList.filter(({id})=>{
-            return id !== itemid;
+        var ids = [];
+        ids.push(itemid);
+        // var newShoppingItemList = this.state.shoppingItemList.filter(({ id }) => {
+        //     return id !== itemid;
+        // });
+        // this.setState({ 'shoppingItemList': newShoppingItemList })
+
+        var deleteNote = new Request('http://localhost:3001/allNotes/deleteNote', {
+            method: 'delete',
+            redirect: 'follow',
+            body:JSON.stringify({'ids':[itemid]}),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
         });
-        this.setState({'shoppingItemList':newShoppingItemList})
+        this.setState({ isLoading: true });
+        fetch(deleteNote)
+            .then(response => response.json())
+            .then(data => {
+                if (data)
+                    this.setState({ shoppingItemList: data, isLoading: false })
+                else
+                    this.setState({ isLoading: false, isError: true })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ isLoading: false, isError: true })
+            })
     }
 
-    displayShoppingList = ()=>{
-        if(this.state.shoppingItemList.length!==0){
+    displayShoppingList = () => {
+        if (this.state.shoppingItemList.length !== 0) {
             console.log('items=>');
-            return(
+            return (
                 this.state.shoppingItemList.map((item) => {
-                console.log(item);
-                return (<ItemBox content={item} onEdit={this.editItem} onDelete = {this.deleteItem} onDoneToggle = {this.onDoneToggle} ></ItemBox>)
-            }))
-        }else{
-            return(<div className='zeroState'>
+                    console.log(item);
+                    return (<ItemBox content={item} onEdit={this.editItem} onDelete={this.deleteItem} onDoneToggle={this.onDoneToggle} ></ItemBox>)
+                }))
+        } else {
+            return (<div className='zeroState'>
                 <div className='zeroStateText'>
-                <i className='icon massive cart plus'></i>
+                    <i className='icon massive cart plus'></i>
                 </div>
                 <div className='zeroStateText'>
-                    <br/>yaay! done with Shopping! <br/>
+                    <br />yaay! done with Shopping! <br />
                     Add items to continue Shopping!
                 </div>
             </div>)
         }
     }
 
+    componentDidMount() {
+        var getAllNotes = new Request('http://localhost:3001/allNotes/getAllNotes', {
+            method: 'GET',
+            redirect: 'follow',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
+        });
+        this.setState({ isLoading: true });
+        fetch(getAllNotes)
+            .then(response => response.json())
+            .then(data => {
+                if (data)
+                    this.setState({ shoppingItemList: data, isLoading: false })
+                else
+                    this.setState({ isLoading: false, isError: true })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ isLoading: false, isError: true })
+            })
+    }
+
     render() {
         return (<div className='ShoppingList'>
             <div className='header'>
-                <div className="ui vertical animated button primary" onClick ={()=>{this.addItemToList()}} >
+                <div className="ui vertical animated button primary" onClick={() => { this.addItemToList() }} >
                     <div className="hidden content">Add</div>
                     <div className="visible content">
                         <i className="plus icon"></i>
